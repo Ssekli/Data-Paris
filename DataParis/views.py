a30dcfec34f4f230683f38bdcabbd667acf474cd
Ssekli <111066892+Ssekli@users.noreply.github.com>
2023-04-06 13:00:41 +0200

    Add files via upload

--- DataParis/views.py
+++ DataParis/views.py
@@ -47,6 +47,7 @@ def home(request):
 
 def Question_1(request) :
 
+    #lecture de la base
     df = pd.read_csv("E:/Projet_Data/Data/que-faire-a-paris-.csv", sep= ';', header = 0)
     df_propre = df.copy()
 
@@ -62,13 +63,7 @@ def Question_1(request) :
     #df_propre.drop(columns = ["url",  ],  inplace = True)
     df_propre.drop(df_propre.columns.difference(['id', 'title', 'date_start', 'date_end', 'tags', 'address_name', 'address_street', 'address_zipcode', 'lat_lon', 'price_type']), axis=1, inplace=True)
 
-
-    #print(df_propre.isnull().values.any())
-
-    #print(df_propre)
-
-    arrondissement = [75001, 75002, 75003, 75004, 75005, 75006, 75007, 75008, 75009, 75010, 75011, 75012, 75013, 75014, 75015, 75016, 75017, 75018, 75019, 75020]
-
+    #creation  de la base pour arrondissement et prix
     df_price_rip = df_propre["price_type"]
     df_arr = df_propre["address_zipcode"]
 
@@ -78,25 +73,22 @@ def Question_1(request) :
     df_arrondissement = pd.concat([df_price, df_arr], axis = 1)
     
 
-    #df_arrondissement = df_arrondissement.reset_index()
-
-    #print(df_arrondissement.value_counts())
-
-    #df_arrondissement.hist(column= "address_zipcode", by = "price_type")
-    # plt.show()
-
-    #sns.countplot(x = "address_zipcode", hue ="price_type", data = df_arrondissement)
+    #filtre pour enlever les valeurs trop peu importantes
     threshold = 10
     zip_counts = df_arrondissement ["address_zipcode"].value_counts()
     valid_zips = zip_counts[zip_counts >= threshold].index
     df_valid = df_arrondissement[df_arrondissement["address_zipcode"].isin(valid_zips)]
     print(df_valid.value_counts())
+
+    #creation du df pour l'affichage
     df_show = df_valid.value_counts()
     df_show = df_show.to_frame()
     df_show = df_show.sort_values(by = "address_zipcode", ascending = True)
     df_show = df_show.transpose()    
     df_html = df_show.to_html()
 
+
+    #parametres du countplot
     plt.switch_backend('AGG')       # added Katsuji
 
     mpl.rcParams['axes.labelsize'] = 20
@@ -106,6 +98,7 @@ def Question_1(request) :
     #plt.figure(figsize=(30,30))
     plt.figure(figsize=(10, 8))     # modified Katsuji
 
+    #creation du countplot
     fig = sns.countplot(x="address_zipcode", hue="price_type", data = df_valid, order = ["75001", "75002", "75003", "75004", "75005", "75006", "75007", "75008", "75009", "75010", "75011", "75012", "75013", "75014", "75015", "75016", "75017", "75018", "75019", "75020"])
     #fig.set(title = " Nombre d'évènements gratuits ou payants pas arrondissement ")
     plt.title(" Nombre d'évènements gratuits ou payants par arrondissement ", fontsize = 20)
